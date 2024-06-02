c06e8f2f96cf552468a05e5f1bbb7730fd9ee6a3
pawurb <contact@pawelurbanek.com>
2024-06-02 14:12:28 +0200

    initial calculations

--- test/BundleExecutor-before.t.sol
+++ test/BundleExecutor-before.t.sol
@@ -10,20 +10,47 @@ contract ExecutorTest is Test {
     IUniV2Pair uniPool = IUniV2Pair(0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11);
     IUniV2Pair sushiPool =
         IUniV2Pair(0xC3D03e4F041Fd4cD388c549Ee2A29a9E5075882f);
-    IERC20 weth = IERC20(0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2);
-    IERC20 dai = IERC20(0x6b175474e89094c44da98b954eedeac495271d0f);
+    IERC20 weth = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IERC20 dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
     FlashBotsMultiCall public executor;
 
     function setUp() public {
         executor = new FlashBotsMultiCall(me);
+        uint256 wethAmountIn = 0.1 ether;
+        (uint112 uniDaiReserve, uint112 uniWethReserve, ) = uniPool
+            .getReserves();
+        (uint112 sushiDaiReserve, uint112 sushiWethReserve, ) = sushiPool
+            .getReserves();
+
+        uint256 daiAmountOut = getAmountOut(
+            wethAmountIn,
+            sushiWethReserve,
+            sushiDaiReserve
+        );
+
+        uint256 wethAmountOut = getAmountOut(
+            daiAmountOut,
+            uniDaiReserve,
+            uniWethReserve
+        );
+
+        console.log(daiAmountOut);
+        console.log(wethAmountOut);
     }
 
-    function test_uniswapWeth() public {}
+    function test_uniswapWeth() public {
+        console.log("dupa1");
+    }
 
     function getAmountOut(
-        address pool,
-        uint256 amountIn
-    ) internal view returns (uint256) {
-        return 0;
+        uint256 amountIn,
+        uint256 reserveIn,
+        uint256 reserveOut
+    ) internal pure returns (uint256 amountOut) {
+        uint256 amountInWithFee = amountIn * 997;
+        uint256 numerator = amountInWithFee * reserveOut;
+        uint256 denominator = reserveIn * 1000 + amountInWithFee;
+        amountOut = numerator / denominator;
+        return amountOut;
     }
 }
