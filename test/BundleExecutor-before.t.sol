9fa13a6a8cd39af3169546ca8581e95449610d11
pawurb <contact@pawelurbanek.com>
2024-06-03 13:50:13 +0200

    wipwerk

--- test/BundleExecutor-before.t.sol
+++ test/BundleExecutor-before.t.sol
@@ -1,64 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 import {FlashBotsMultiCall} from "../src/BundleExecutor-before.sol";
-import {IUniV2Pair, IERC20} from "./shared.sol";
+import {BaseTest} from "./shared.sol";
 
-// ETH PRICE $3800.0
-contract ExecutorTest is Test {
-    address constant me = address(1);
-    IUniV2Pair uniPool = IUniV2Pair(0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11);
-    IUniV2Pair sushiPool =
-        IUniV2Pair(0xC3D03e4F041Fd4cD388c549Ee2A29a9E5075882f);
-    IERC20 weth = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-    IERC20 dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
-    FlashBotsMultiCall public executor;
-
-    function setUp() public {
-        vm.prank(me);
-        executor = new FlashBotsMultiCall(me);
-
-        vm.store(
-            address(uniPool),
-            bytes32(uint256(8)), // getReserves slot
-            0x665c6fc30000000000726da71f957be90350000000069edd451a8cac85b3f053
-        );
-
-        vm.store(
-            address(sushiPool),
-            bytes32(uint256(8)), // getReserves slot
-            0x665c6fcf00000000004d4a487a40a07d962e0000000461F5535113259E306380
-        );
-
-        vm.deal(me, 0.1 ether);
-        deal(address(weth), address(uniPool), 2110830143201023165264);
-        deal(address(dai), address(uniPool), 8003770531868479419838547);
-        deal(address(weth), address(sushiPool), 1425751956250754389550);
-        deal(address(dai), address(sushiPool), 5298298283195761674969984);
-        deal(address(weth), address(executor), 0.1 ether);
-    }
-
+contract ExecutorBeforeTest is BaseTest {
     function test_uniswapWeth() public {
-        uint256 wethAmountIn = 0.1 ether;
-        (uint112 uniDaiReserve, uint112 uniWethReserve, ) = uniPool
-            .getReserves();
-        (uint112 sushiDaiReserve, uint112 sushiWethReserve, ) = sushiPool
-            .getReserves();
+        vm.prank(me);
+        FlashBotsMultiCall executor = new FlashBotsMultiCall(me);
 
-        uint256 daiAmountOut = getAmountOut(
-            wethAmountIn,
-            uniWethReserve,
-            uniDaiReserve
-        );
-
-        uint256 wethAmountOut = getAmountOut(
-            daiAmountOut,
-            sushiDaiReserve,
-            sushiWethReserve
-        );
-        console.log("wethAmountOut");
-        console.log(wethAmountOut);
+        deal(address(weth), address(executor), 0.1 ether);
 
         bytes memory payload1 = abi.encodeWithSignature(
             "swap(uint256,uint256,address,bytes)",
@@ -70,8 +22,8 @@ contract ExecutorTest is Test {
 
         bytes memory payload2 = abi.encodeWithSignature(
             "swap(uint256,uint256,address,bytes)",
-            wethAmountOut,
             0,
+            wethAmountOut,
             address(executor),
             ""
         );
@@ -84,23 +36,14 @@ contract ExecutorTest is Test {
         payloads[0] = payload1;
         payloads[1] = payload2;
 
-        // 100000000000000
         vm.prank(me);
-        executor.uniswapWeth(wethAmountIn, 0, targets, payloads);
+        executor.uniswapWeth(wethAmountIn, 100000000000000, targets, payloads);
 
         uint256 wethBalanceAfter = weth.balanceOf(address(executor));
+        console.log("wethBalanceAfter");
         console.log(wethBalanceAfter);
-    }
-
-    function getAmountOut(
-        uint256 amountIn,
-        uint256 reserveIn,
-        uint256 reserveOut
-    ) internal pure returns (uint256 amountOut) {
-        uint256 amountInWithFee = amountIn * 997;
-        uint256 numerator = amountInWithFee * reserveOut;
-        uint256 denominator = reserveIn * 1000 + amountInWithFee;
-        amountOut = numerator / denominator;
-        return amountOut;
+        uint256 wethProfit = wethBalanceAfter - wethAmountIn;
+        console.log("wethProfit");
+        console.log(wethProfit);
     }
 }
--- test/shared.sol
+++ test/shared.sol
@@ -1,5 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import "forge-std/Test.sol";
 
 interface IERC20 {
     event Approval(
@@ -44,4 +45,72 @@ interface IUniV2Pair {
             uint112 _reserve1,
             uint32 _blockTimestampLast
         );
+}
+
+contract BaseTest is Test {
+    address me = address(1);
+    IUniV2Pair uniPool = IUniV2Pair(0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11);
+    IUniV2Pair sushiPool =
+        IUniV2Pair(0xC3D03e4F041Fd4cD388c549Ee2A29a9E5075882f);
+    IERC20 weth = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IERC20 dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+
+    uint256 constant wethAmountIn = 0.1 ether;
+    uint256 daiAmountOut = 0;
+    uint256 wethAmountOut = 0;
+
+    function setUp() public {
+        vm.store(
+            address(uniPool),
+            bytes32(uint256(8)), // getReserves slot
+            0x665c6fc30000000000726da71f957be90350000000069edd451a8cac85b3f053
+        );
+
+        vm.store(
+            address(sushiPool),
+            bytes32(uint256(8)), // getReserves slot
+            0x665c6fcf00000000004d4a487a40a07d962e0000000453229E2B8F0ABE706380
+        );
+
+        deal(address(weth), address(uniPool), 2110830143201023165264);
+        deal(address(dai), address(uniPool), 8003770531868479419838547);
+        deal(address(weth), address(sushiPool), 1425751956250754389550);
+        deal(address(dai), address(sushiPool), 5228298283195761674969984);
+
+        console.log("wethAmountIn");
+        console.log(wethAmountIn);
+
+        (uint112 uniDaiReserve, uint112 uniWethReserve, ) = uniPool
+            .getReserves();
+        (uint112 sushiDaiReserve, uint112 sushiWethReserve, ) = sushiPool
+            .getReserves();
+
+        daiAmountOut = UniswapHelper.getAmountOut(
+            wethAmountIn,
+            uniWethReserve,
+            uniDaiReserve
+        );
+
+        wethAmountOut = UniswapHelper.getAmountOut(
+            daiAmountOut,
+            sushiDaiReserve,
+            sushiWethReserve
+        );
+        console.log("wethAmountOut");
+        console.log(wethAmountOut);
+    }
+}
+
+library UniswapHelper {
+    function getAmountOut(
+        uint256 amountIn,
+        uint256 reserveIn,
+        uint256 reserveOut
+    ) external pure returns (uint256 amountOut) {
+        uint256 amountInWithFee = amountIn * 997;
+        uint256 numerator = amountInWithFee * reserveOut;
+        uint256 denominator = reserveIn * 1000 + amountInWithFee;
+        amountOut = numerator / denominator;
+        return amountOut;
+    }
 }
